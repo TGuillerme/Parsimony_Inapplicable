e6bb3749bfaf7d124b0762cfbf7b295707b7bf89
TGuillerme <guillert@tcd.ie>
2016-01-18 12:20:14 +0000

    inapplicable INIT

---
+++ Analysis/Counting_inapplicable.R
@@ -0,0 +1,6 @@
+#Counting the number of characters with inapplicable data from the Biology Letters matrices.
+
+library(Claddis)
+
+setwd("../Data/Matrices")
+test <- ReadMorphNexus(list.files()[1])
---
+++ Functions/install.inapplicable.R
@@ -0,0 +1,1 @@
+#Install the inapplicable package.
